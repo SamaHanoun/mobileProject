ba40b15501b84bd646bc13b03d6fec9b1071c998
Ghoul67 <momen.sh.amd@gmail.com>
2024-06-12 09:46:35 +0300

    momen v0.4

--- app/src/main/java/com/example/carrentalproject/MainActivity.java
+++ app/src/main/java/com/example/carrentalproject/MainActivity.java
@@ -2,14 +2,28 @@ package com.example.carrentalproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,6 +54,61 @@ public class MainActivity extends AppCompatActivity {
 
                 String email = edtEmail.getText().toString();
                 String password = edtPassword.getText().toString();
+
+                String url = "http://localhost:80/comp4310 mobile/signIn.php";
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please enter email and password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+
+                JSONObject data = new JSONObject();
+
+                try {
+                    data.put("email", email);
+                    data.put("password", password);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Log.d("jsonObjectData", data.toString());
+                JsonRequest jsonRequest = new JsonRequest(Request.Method.POST, url, data.toString(),
+                        new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("jsonObjectData", response.toString());
+                        try {
+                            JSONObject obj = response.getJSONArray(0).getJSONObject(0);
+
+                            String uid = obj.getString("uid");
+                            Log.d("jsonObjectData", uid);
+                            Intent intent = new Intent(
+                                    MainActivity.this,
+                                    HomePage.class
+                            );
+                            startActivity(intent);
+
+                        } catch (JSONException exception) {
+                            Log.d("jsonObjectData", exception.toString());
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+                        Log.d("jsonObjectData", error.toString());
+                    }
+
+                }) {
+                    @Override
+                    protected Response parseNetworkResponse(NetworkResponse networkResponse) {
+                        return null;
+                    }
+                };
+                queue.add(jsonRequest);
 
 
             });
